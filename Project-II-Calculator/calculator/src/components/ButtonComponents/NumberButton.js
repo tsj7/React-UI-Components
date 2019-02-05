f161d582c949eafb7940fe9491cabd9d05b68cc8
Tyge Johnson <tygejohnson@Tyge-Johnsons-MacBook-Pro.local>
2019-02-05 08:38:16 -0700

    add NumberButton.js

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Button.css';
+
+const NumberButton = () => {
+  return (
+    <button className={`number-button ${props.bgColor}`}>{props.text}</button>
+  );
+};
+
+export default NumberButton;
